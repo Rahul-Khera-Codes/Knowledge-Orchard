4ad1dd93e9e531108f853c14529c75f64aaec7d2
Rahul-Khera-Codes <rahul.excel2011@gmail.com>
2024-12-05 18:23:56 +0100

    updated files to align with project goals

---
+++ nuxt.config.js
@@ -0,0 +1,94 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'digital-garden',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['@/assets/css/tailwind.css'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
+    '@nuxt/postcss8',
+    '@nuxtjs/color-mode',
+    '@nuxt/image',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      name: "Leigh's Digital Garden",
+      short_name: "Leigh's Digital Garden",
+      lang: 'en',
+      description: 'A digital garden for Leigh',
+      background_color: '#fafafa',
+      theme_color: '#fafafa',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/favicon.ico',
+          sizes: '512x512',
+          type: 'image/x-icon',
+        },
+      ],
+    },
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {
+    markdown: {
+      prism: {
+        theme: 'prism-themes/themes/prism-material-oceanic.css',
+      },
+    },
+    nestedProperties: ['notes.tag'],
+  },
+
+  colorMode: {
+    preference: 'light', // default value of $colorMode.preference
+    fallback: 'light',
+    classSuffix: '',
+  },
+
+  image: {
+    // Options
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
+}
